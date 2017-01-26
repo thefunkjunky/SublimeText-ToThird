26b9a487a452b693a413862143fc6769ed94f9f3
Garrett Anderson <gaanderson@gaanderson-macbookpro.roam.corp.google.com>
2017-01-25 20:59:42 -0800

    Abandoned efforts to write sentence-parser, and decided to use nltk's tokenizer instead.

--- to-third.py
+++ to-third.py
@@ -1,16 +1,28 @@
-import sublime
-import sublime_plugin
+import sys
 import json
 import re
+import imp
+
+import sublime
+import sublime_plugin
+
 
 settings = sublime.load_settings("ToThird.sublime-settings")
 package_path = sublime.packages_path()
+sys.path.insert(0, "{}/ToThird/lib".format(
+    package_path))
+
+from nltk import download as nltk_download
+# TODO: Test if nltk data exists, if not, find way to automate nltk_download()
+# TODO: Dont forget to set 'NLTK_DATA' env variable to data location
+# nltk_download()
+from nltk.tokenize import sent_tokenize
+
 wordmap_location = "{}/ToThird/ToThird-wordmap.json".format(
     package_path)
 with open(wordmap_location, "r") as mapfile:
     mapdata = mapfile.read()
     WORDMAP = json.loads(mapdata)
-
 
 class ToThirdPerson(sublime_plugin.TextCommand):
     """ Main class for To Third (person) sublime text 3 package """
@@ -36,10 +48,30 @@ class ToThirdPerson(sublime_plugin.TextCommand):
         operations_ordered = ['conjunctions', 'multi-word', 'single-word']
         for op in operations_ordered:
             for key, value in WORDMAP[op].items():
-                # print("key {} value {}".format(key, value))
-                # print("\nText: {}".format(str(text)))
                 reobject = re.compile(key, re.I | re.M)
                 text = reobject.sub(value, str(text))
 
-        return text
+        fixed_text = self.fix_case(text)
+        return fixed_text
+
+    def fix_case(self, text):
+        """  Parses sentences and fixes case with str.capitalize() """
+
+        # What I got so far for the delimiter_re:
+        # r"(?! )([a-z|A-Z|, |'|"]*)(?=[.|?|!|\n][ |\n]*)"
+
+        # Even better, find a way to steal
+        # http://www.nltk.org/_modules/nltk/tokenize/punkt.html
+
+        # example of a st3 plugin installing a 3rd party library:
+        # https://github.com/sergeche/emmet-sublime/blob/master/emmet/pyv8loader.py
+
+        # delimiters_re = r"(.+\w+)(?=\.)"
+        # split_text = re.split(delimiters_re, text, re.M)
+        sentences = sent_tokenize(text)
+        print(sentences)
+        fixed_text = ". ".join(sentence.capitalize()
+                                for sentence in sentences)
+
+        return fixed_text
 
