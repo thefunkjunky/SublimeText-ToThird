9fcca5755773706132bd275dd0dfe78499d8d7fd
Garrett Anderson <gaanderson@gaanderson-macbookpro.roam.corp.google.com>
2017-01-27 20:22:10 -0800

    Solved the spaces between tokenized sentences issue.

--- to-third.py
+++ to-third.py
@@ -34,8 +34,24 @@ with open(abbrev_location, 'r') as abbrev_file:
 # Must use <= v3.2 of nltk, as sublime text 3 uses old python 3.3
 from nltk import download as nltk_download
 from nltk.tokenize import sent_tokenize
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+from nltk.tokenize.punkt import (PunktSentenceTokenizer,
+                                PunktParameters,
+                                PunktLanguageVars)
 nltk_download("punkt")
+
+class CustomLanguageVars(PunktLanguageVars):
+    """ Customized Punkt CustomLanguageVars _period_context_fmt 
+    for the purpose of retaining trailing spaces (and newlines?)
+    in tokenized sentences"""
+    _period_context_fmt = r"""
+        \S*                          # some word material
+        %(SentEndChars)s             # a potential sentence ending
+        \s*                       #  <-- THIS is what I changed
+        (?=(?P<after_tok>
+            %(NonWord)s              # either other punctuation
+            |
+            (?P<next_tok>\S+)     #  <-- Normally you would have \s+ here
+        ))"""
 
 
 def punkt_tokenize(text):
@@ -46,7 +62,8 @@ def punkt_tokenize(text):
         punkt_param = PunktParameters()
         abbreviation = ABBREV
         punkt_param.abbrev_types = set(abbreviation)
-        tokenizer = PunktSentenceTokenizer(punkt_param)
+        tokenizer = PunktSentenceTokenizer(punkt_param,
+                                            lang_vars=CustomLanguageVars())
     tokenized_text = tokenizer.tokenize(text)
     return tokenized_text
 
@@ -64,9 +81,7 @@ def sentence_cleanup(sentences):
     ellipses_re = r"(\.\.\.)"
     # TODO: This regex needs to be fixed.
     # Catch alphanumeric lists (1. blah, a) Blah, 123. Blah)
-    alphanum_list_re = r"(?<!\w)(\A|\n)([a-zA-Z0-9]+[\.|\)]{1})(?!\.)"
-    # Final regex for finding first character to capitalize
-    char_to_capitalize_re = r"[a-zA-Z]"
+    alphanum_list_re = r"(?<!\w)(\A|\n)([a-zA-Z0-9]+[\.|\)]{1})(?!\.)( )*"
 
     # teststring = "1. Mr. Jennings is testing a numbered line."
     # print("teststring: {}".format(re.split(alphanum_list_re, teststring)))
@@ -75,10 +90,6 @@ def sentence_cleanup(sentences):
     # Flatten nested lists
     sen_split_ellipses = list(chain(*sen_split_ellipses))
     # print("sen_split_ellipses: {}".format(sen_split_ellipses))
-
-    for index, string in enumerate(sen_split_ellipses):
-        if string == "...":
-            pass
 
     sen_split_alphanum_list = [re.split(alphanum_list_re, sentence)
                                 for sentence in sentences]
@@ -96,6 +107,9 @@ def sentence_cleanup(sentences):
                     if re.search("[a-zA-Z]", sentence)
                     else sentence.capitalize()
                     for sentence in sen_cleaned)
+
+    # TODO: capitalize title abbreviations (Mr., Mrs., etc.)
+
     return fixed_text
 
 
@@ -133,6 +147,8 @@ class ToThirdPerson(sublime_plugin.TextCommand):
     def fix_case(self, text):
         """  Parses sentences and fixes case """
 
+        # TODO: Read the following and figure out how to preserve spaces:
+        # TODO: http://stackoverflow.com/questions/33139531/preserve-empty-lines-with-nltks-punkt-tokenizer
         sentences = punkt_tokenize(text)
         print("Punkt sentences: {}".format(sentences))
 
