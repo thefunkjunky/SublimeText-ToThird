edfb196dfbe3e6d3020491328bb402e84a0e2f4f
Garrett Anderson <gaanderson@gaanderson-macbookpro.roam.corp.google.com>
2017-01-27 19:01:39 -0800

    Abstracted some processes into modular functions.  Still working on regexs

--- to-third.py
+++ to-third.py
@@ -10,24 +10,90 @@ import sublime_plugin
 
 
 settings = sublime.load_settings("ToThird.sublime-settings")
+
+# Set library PATH, NLTK Data Directory, load WORDMAP and ABBREV, etc.
 package_path = sublime.packages_path()
 sys.path.insert(0, "{}/ToThird/lib".format(
     package_path))
 NLTK_DATA_DIR = "{}/ToThird/lib/nltk-data".format(
     package_path)
-
-# Must use <= v3.2 of nltk, as sublime text uses old python 3.3
-from nltk import download as nltk_download
-# TODO: Test if nltk data exists, if not, automate nltk_download('punkt')
-# TODO: Dont forget to set 'NLTK_DATA' env variable to data location
-nltk_download("punkt")
-from nltk.tokenize import sent_tokenize
-
 wordmap_location = "{}/ToThird/ToThird-wordmap.json".format(
     package_path)
-with open(wordmap_location, "r") as mapfile:
+abbrev_location = "{}/ToThird/ToThird-abbrevs.json".format(
+    package_path)
+
+with open(wordmap_location, 'r') as mapfile:
     mapdata = mapfile.read()
     WORDMAP = json.loads(mapdata)
+with open(abbrev_location, 'r') as abbrev_file:
+    abbrevdata = abbrev_file.read()
+    ABBREV = json.loads(abbrevdata)
+
+
+# Import NLTK Punkt and download training data file
+# Must use <= v3.2 of nltk, as sublime text 3 uses old python 3.3
+from nltk import download as nltk_download
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+nltk_download("punkt")
+
+
+def punkt_tokenize(text):
+    """ Method to init Punkt, set params, and return tokenized text """
+    try:
+        tokenizer
+    except NameError:
+        punkt_param = PunktParameters()
+        abbreviation = ABBREV
+        punkt_param.abbrev_types = set(abbreviation)
+        tokenizer = PunktSentenceTokenizer(punkt_param)
+    tokenized_text = tokenizer.tokenize(text)
+    return tokenized_text
+
+def sentence_cleanup(sentences):
+    """ Performs various cleanup operations on a list of sentences
+    parsed by nltk Punkt for the purposes of mitigating errors encountered
+    due to Punkt's limitations in certain edge-cases """
+
+    #####
+    # Regex patterns
+    #####
+    # Catch ellipses ("...")
+    # TODO: This results in following string being capitalized incorrectly
+    # TODO: Note that this wouldn't be needed if alphanum_list_re worked better
+    ellipses_re = r"(\.\.\.)"
+    # TODO: This regex needs to be fixed.
+    # Catch alphanumeric lists (1. blah, A. Blah, 123. Blah)
+    alphanum_list_re = r"(?<!\w)([a-zA-Z0-9]+\.{1})(?!\.)"
+
+    # teststring = "1. Mr. Jennings is testing a numbered line."
+    # print("teststring: {}".format(re.split(alphanum_list_re, teststring)))
+    sen_split_ellipses = [re.split(ellipses_re, sentence)
+                                for sentence in sentences]
+    # Flatten nested lists
+    sen_split_ellipses = list(chain(*sen_split_ellipses))
+    print("sen_split_ellipses: {}".format(sen_split_ellipses))
+
+    for index, string in enumerate(sen_split_ellipses):
+        if string == "...":
+            pass
+
+    sen_split_alphanum_list = [re.split(alphanum_list_re, sentence)
+                                for sentence in sen_split_ellipses]
+    # print("sen_split_alphanum_list: {}".format(sen_split_alphanum_list))
+    sen_cleaned = list(chain(*sen_split_alphanum_list))
+    print("sen_cleaned: {}".format(sen_cleaned))
+    # for sentence in sentences:
+    #     if sentence[-1] == "\n":
+    #         delimiter = "\n"
+    #     else:
+    #         delimiter = " "
+    fixed_text = "".join(sentence[re.search("\S", sentence).start():].capitalize()
+                            if re.search("\S", sentence)
+                            else sentence.capitalize()
+                            for sentence in sen_cleaned)
+    return fixed_text
+
 
 class ToThirdPerson(sublime_plugin.TextCommand):
     """ Main class for To Third (person) sublime text 3 package """
@@ -61,26 +127,12 @@ class ToThirdPerson(sublime_plugin.TextCommand):
         return fixed_text
 
     def fix_case(self, text):
-        """  Parses sentences and fixes case with str.capitalize() """
+        """  Parses sentences and fixes case """
 
-        alphanum_list_re = r'(?<!\w)(.{1}\.)'
-        teststring = "1. they are testing a numbered line."
-        print("teststring: {}".format(re.split(alphanum_list_re, teststring, 1)))
-        sentences = sent_tokenize(text)
-        sen_split_alphanum_list = [re.split(alphanum_list_re, sentence)
-                                    for sentence in sentences]
-        # print("sen_split_alphanum_list: {}".format(sen_split_alphanum_list))
-        sen_cleaned = list(chain(*sen_split_alphanum_list))
-        print("sen_cleaned: {}".format(sen_cleaned))
-        # for sentence in sentences:
-        #     if sentence[-1] == "\n":
-        #         delimiter = "\n"
-        #     else:
-        #         delimiter = " "
-        fixed_text = "".join(sentence[re.search("\S", sentence).start():].capitalize()
-                                if re.search("\S", sentence)
-                                else sentence.capitalize()
-                                for sentence in sen_cleaned)
+        sentences = punkt_tokenize(text)
+        print("Punkt sentences: {}".format(sentences))
+
+        fixed_text = sentence_cleanup(sentences)
 
         print("fixed test: {}".format(fixed_text))
 
